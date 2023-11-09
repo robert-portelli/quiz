571efff1c3df3af84248e954445e0ec328d24ccc
Robert Portelli <github@robertportelli.com>
2023-11-09 10:31:05 -0500

    sorted trivia answer alternatives

--- src/quiz/quiz.py
+++ src/quiz/quiz.py
@@ -1,12 +1,32 @@
 # quiz.py
 
-QUESTIONS = [
-    ("When was the first known use of the word 'quiz'", "1781"),
-    ("Which built-in function can get information from the user", "input"),
-    ("Which keyword do you use to loop over a given list of elements", "for")
-]
+QUESTIONS = {
+    "When was the first known use of the word 'quiz'": ["1781", "1771", "1871", "1881"],
+    "Which built-in function can get information from the user": [
+        "input",
+        "get",
+        "print",
+        "write",
+    ],
+    "Which keyword do you use to loop over a given list of elements": [
+        "for",
+        "while",
+        "each",
+        "loop",
+    ],
+    "What's the purpose of the built-in zip() function": [
+        "To iterate over two or more sequences at the same time",
+        "To combine several strings into one",
+        "To compress several files into one archive",
+        "To get information from the user",
+    ],
+}
 
-for question, correct_answer in QUESTIONS:
+for question, alternatives in QUESTIONS.items():
+    correct_answer = alternatives[0]
+    for alternative in sorted(alternatives):
+        print(f"  - {alternative}")
+
     answer = input(f"{question}? ")
     if answer == correct_answer:
         print("Correct!")
