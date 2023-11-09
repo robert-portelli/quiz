7419031d546b9d153614f5dbe9540d7ed94c9154
Robert Portelli <github@robertportelli.com>
2023-11-09 09:12:34 -0500

    make it easier to manipulate trivia

--- src/quiz/quiz.py
+++ src/quiz/quiz.py
@@ -1,21 +1,14 @@
 # quiz.py
 
-"""repeat yourself a lot"""
+QUESTIONS = [
+    ("When was the first known use of the word 'quiz'", "1781"),
+    ("Which built-in function can get information from the user", "input"),
+    ("Which keyword do you use to loop over a given list of elements", "for")
+]
 
-answer = input("When was the first known use of the word 'quiz'? ")
-if answer == "1781":
-    print("Correct!")
-else:
-    print(f"The answer is '1781', not {answer!r}")
-
-answer = input("Which built-in function can get information from the user? ")
-if answer == "input":
-    print("Correct!")
-else:
-    print(f"The answer is 'input', not {answer!r}")
-
-answer = input("Which keyword do you use to loop over a given list of elements ")
-if answer == "for":
-    print("Correct!")
-else:
-    print(f"The answer is 'input', not {answer!r}")
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
