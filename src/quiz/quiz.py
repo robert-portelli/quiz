2765aa4c510f472dc6eaabce1ed2a0b943aeff36
Robert Portelli <github@robertportelli.com>
2023-11-10 11:57:31 -0500

    Fixes #9: add questions

--- src/quiz/quiz.py
+++ src/quiz/quiz.py
@@ -59,6 +59,18 @@ QUESTIONS = {
         "The computer clock is reset.",
         "The first random number is always 42.",
     ],
+    "When does __name__ == '__main__' equal True in a Python file": [
+        "When the file is run as a script",
+        "When the file is imported as a module",
+        "When the file has a valid name",
+        "When the file only has one function",
+    ],
+    "Which version of Python is the first with TOML support built in": [
+        "3.11", "3.9", "3.10", "3.12",
+    ],
+    "What's the name of the list-like data structure in TOML": [
+        "Array", "List", "Sequence", "Set",
+    ],
 }
 
 num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
