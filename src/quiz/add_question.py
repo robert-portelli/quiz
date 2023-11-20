849111e9023aa932992c23fd4209f3fa444ac9ea
Robert Portelli <github@robertportelli.com>
2023-11-20 17:18:48 -0500

    Fixes #32

--- src/quiz/add_question.py
+++ src/quiz/add_question.py
@@ -1,28 +1,80 @@
+"""
+add_questions.py
+
+This script allows users to add new questions to a TOML file containing
+trivia questions.
+
+The questions are organized by topics, and each question is represented
+as a dictionary.
+
+Usage:
+    - Run the script to add a new question to the specified topic in the
+        QUESTIONS.toml file.
+
+Dependencies:
+    - pathlib: For working with file paths.
+    - tomllib: For parsing and manipulating TOML data.
+    - tomli_w: For writing TOML data.
+    - quiz: Contains the main_loop function used for selecting the topic.
+
+Author:
+    [Your Name]
+"""
+
 import pathlib
 import tomllib
-from string import ascii_lowercase
+from tomli_w import dump
 from quiz import main_loop
 
 QUESTIONS_PATH = pathlib.Path(__file__).parent / "QUESTIONS.toml"
 TRIVIA_TOML = tomllib.loads(QUESTIONS_PATH.read_text())
 
-#topics = dict(zip(ascii_lowercase, TRIVIA_TOML.keys()))
 
 def main():
-    #topic_path = toml_path_to_topic()
-    #new_question = create_question()
-    #append_new_question = write_to_toml(topic_path, new_question)
-    pass
+    """
+    Main function to add a new question to the specified topic in the
+        QUESTIONS.toml file.
+
+    Steps:
+        1. Get the list of existing questions for the selected topic.
+        2. Create a new question dictionary.
+        3. Append the new question to the list of existing questions.
+        4. Update the QUESTIONS.toml file with the new data.
+    """
+    questions_list = path_to_list_of_dicts()
+    new_question_dict = create_question()
+    questions_list.append(new_question_dict)
+    with open(QUESTIONS_PATH, "wb") as f:
+        dump(TRIVIA_TOML, f)
+
+
+def path_to_list_of_dicts():
+    """
+    Return the list of dictionaries representing questions for the
+        selected topic.
+
+    Returns:
+        List[Dict]: A list of dictionaries where each dictionary represents
+            a question.
+    """
+    topic_label = main_loop(
+        question="Which topic do you want to add a question to",
+        alternatives=TRIVIA_TOML.keys(),
+    )[0]
+    return TRIVIA_TOML[topic_label]["questions"]
+
 
 def create_question():
     """
-    create a dictionary with items {
-        'question': str,
-        'answers': List,
-        'alternatives': List,
-        'hint': None
-        'explanation': None
-    }
+    Create a dictionary representing a new trivia question.
+
+    Returns:
+        Dict: A dictionary with the following items:
+            - 'question': str,
+            - 'answers': List,
+            - 'alternatives': List,
+            - 'hint': None,
+            - 'explanation': None
     """
     question = input("Enter your question prompt: ")
     answers = input("Enter one correct answer at a time: ")
@@ -31,29 +83,12 @@ def create_question():
     explanation = input("Offer an explanation? - enter to skip")
     return {
         "question": question,
-        "answers": answers,
-        "alternatives": alternatives,
+        "answers": list([answers]),
+        "alternatives": list([alternatives]),
         "hint": hint,
-        "explanation": explanation
+        "explanation": explanation,
     }
 
 
-
-def toml_path_to_topic():
-    """
-    Return the path to a list of dictionaries where:
-        each dictionary is of keys: question, answers, alternatives,
-        hint, explanation.
-    """
-    topic_label = main_loop(
-        question="Which topic do you want to add a question to",
-        alternatives=TRIVIA_TOML.keys(),
-    )[0]
-    # append new question dictionaries here
-    # THIS IS "TOPIC QUESTIONS PATH"
-    return TRIVIA_TOML[topic_label]['questions']
-
-
 if __name__ == "__main__":
-    topic_path = toml_path_to_topic()
-    new_question = create_question()
+    main()
